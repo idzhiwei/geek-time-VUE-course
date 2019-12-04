97ef0fe832595d597edee3109cc524b6b67cfadb
jam <jamis404@163.com>
2019-12-05 00:07:51 +0800

    单独引入组件还是很烦啊，改为按需引入，使用 babel-plugin-import

--- ant-design-vue-pro/src/main.js
+++ ant-design-vue-pro/src/main.js
@@ -1,18 +1,15 @@
 import Vue from "vue";
-// import Antd from "ant-design-vue";
-import Button from "ant-design-vue/lib/button";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-// import "ant-design-vue/dist/antd.less";
-import "ant-design-vue/lib/button/style"
+import { Button } from "ant-design-vue";
 
 Vue.config.productionTip = false;
 
-Vue.use(Button)
+Vue.use(Button);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
